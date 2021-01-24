f4eed57ab5b760c08a33f82658b39a2f07b168ef
ekaygusuz <emrehankaygusuz@gmail.com>
2021-01-24 19:42:26 +0200

    vo.5

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
-const https      = require("https");
 const app        = express();
 const mongoose   = require("mongoose");
 app.set("view engine", "ejs");
